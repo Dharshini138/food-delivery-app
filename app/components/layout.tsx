0703125534e5e4ba3f40958172236f015c9e14ce
Dharshini138 <dharshinim22mss008@skasc.ac.in>
2025-04-04 10:58:48 +0530

    Update layout.tsx

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -1,69 +1,17 @@
-/* SPDX-FileCopyrightText: 2014-present Kriasoft */
-/* SPDX-License-Identifier: MIT */
+import { Routes, Route } from "react-router-dom";
+import Layout from "@/layout/Layout";
+import Home from "@/pages/Home";
+import Login from "@/pages/Login";
+import Signup from "@/pages/Signup";
 
-import { Box, GlobalStyles } from "@mui/joy";
-import { Fragment, Suspense } from "react";
-import { Outlet } from "react-router-dom";
-import { Logo } from "./logo";
-import { Sidebar } from "./sidebar";
-import { Toolbar } from "./toolbar";
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<Signup />} />
+    </Route>
+  </Routes>
+);
 
-/**
- * The main application layout.
- */
-export function MainLayout(): JSX.Element {
-  return (
-    <Fragment>
-      <GlobalStyles
-        styles={{
-          "#root": {
-            display: "grid",
-            gridTemplateColumns: "auto 1fr",
-            gridTemplateRows: "auto 1fr",
-            height: "100dvh",
-          },
-        }}
-      />
-      <Toolbar sx={{ gridArea: "1 / 2 / 2 / -1" }} />
-
-      <Sidebar sx={{ gridArea: "1 / 1 / -1 / 2" }} />
-      <Logo sx={{ gridArea: "1 / 1 / 2 / 2", zIndex: 100 }} />
-
-      <Box sx={{ gridArea: "1 / 2 / -1 / -1", pt: "60px" }}>
-        <Suspense>
-          <Outlet />
-        </Suspense>
-      </Box>
-    </Fragment>
-  );
-}
-
-/**
- * The minimal app layout to be used on pages such Login/Signup,
- * Privacy Policy, Terms of Use, etc.
- */
-export function BaseLayout(): JSX.Element {
-  return (
-    <Fragment>
-      <GlobalStyles
-        styles={{
-          "#root": {
-            display: "grid",
-            gridTemplateColumns: "1fr",
-            minHeight: "100vh",
-          },
-        }}
-      />
-
-      <Box sx={{ gridArea: "1 / 1 / 2 / 2 " }}>
-        <Logo />
-      </Box>
-
-      <Box sx={{ gridArea: "1 / 1 / -1 / -1", pt: "60px" }}>
-        <Suspense>
-          <Outlet />
-        </Suspense>
-      </Box>
-    </Fragment>
-  );
-}
+export default App;
